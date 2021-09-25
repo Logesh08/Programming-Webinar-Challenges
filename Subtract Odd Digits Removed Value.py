646a11e89212a2a049959f8b384e7ac724582009
Logesh Krishna <logeshkrishna08@gmail.com>
2021-09-25 21:28:39 +0530

    Sat 09/25/2021

---
+++ Subtract Odd Digits Removed Value.py
@@ -0,0 +1,39 @@
+# Subtract Odd Digits Removed Value
+
+# The program must accept an integer N as the input. The program must find X which is the value obtained from N after removing the odd digits. The program must print the value of N-X as the output.
+
+# Boundary Condition(s):
+# 1 <= N <= 10^9
+
+# Input Format:
+# The first line contains N.
+
+# Output Format:
+# The first line contains an integer value.
+
+# Example Input/Output 1:
+# Input:
+# 12345
+
+# Output:
+# 12321
+
+# Explanation:
+# Here the value of N is 12345.
+# The integer obtained by removing the odd digits in 12345 is 24.
+# The value of N-X is 12321 (12345-24), which is printed as the output.
+
+# Example Input/Output 2:
+# Input:
+# 4162
+
+# Output:
+# 3700
+
+
+n=input().strip()
+val="0"
+for c in n:
+    if int(c)%2==0:
+        val+=c 
+print(int(n)-int(val))
